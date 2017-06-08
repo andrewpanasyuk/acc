f4080423514ae31df3a38424ea8167ad0facd338
volnitsky <dmitry@volnitsky.com>
2017-06-08 19:36:39 +0300

    add Employee(model,service,repository)

--- src/main/java/ua/com/foxminded/accountingsystem/model/EmployeeField.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/EmployeeField.java
@@ -1,5 +1,37 @@
 package ua.com.foxminded.accountingsystem.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 
-public class EmployeeField {
+
+@Entity
+@Table(name = "employee_field")
+public class EmployeeField implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(name = "field_name", nullable = false)
+    private String name;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
