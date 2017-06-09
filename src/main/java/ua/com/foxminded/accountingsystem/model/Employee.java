b848e135a4cec50bcd155d460d6683f6df487712
volnitsky <dmitry@volnitsky.com>
2017-06-09 05:35:57 +0300

    add remove employee and extra_fields to view

--- src/main/java/ua/com/foxminded/accountingsystem/model/Employee.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/Employee.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -17,7 +18,7 @@ import java.util.List;
 @Table(name = "employee")
 public class Employee implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
     @Column(name = "first_name")
     private String firstName;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR,
     last_name VARCHAR,
     PRIMARY KEY (id),
 );
 
 CREATE TABLE employee_field (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     field_name VARCHAR,
     PRIMARY KEY (id),
 );
 
 CREATE TABLE employee_field_value (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     employee_fk INT,
     employee_field_fk INT,
     field_value VARCHAR,
