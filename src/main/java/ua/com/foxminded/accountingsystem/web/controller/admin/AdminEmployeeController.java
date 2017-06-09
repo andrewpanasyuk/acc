fb06d1a8d5c48d095876d32502d3d21bf9682c66
volnitsky <dmitry@volnitsky.com>
2017-06-09 05:35:57 +0300

    add remove employee and extra_fields to view

--- src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminEmployeeController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminEmployeeController.java
@@ -3,11 +3,22 @@ package ua.com.foxminded.accountingsystem.web.controller.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.com.foxminded.accountingsystem.model.Currency;
 import ua.com.foxminded.accountingsystem.model.Employee;
+import ua.com.foxminded.accountingsystem.model.Order;
+import ua.com.foxminded.accountingsystem.model.Price;
+import ua.com.foxminded.accountingsystem.model.Service;
 import ua.com.foxminded.accountingsystem.service.EmployeeService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -18,16 +29,43 @@ public class AdminEmployeeController {
 
     @Autowired
     public AdminEmployeeController(EmployeeService employeeService) {
-        this.employeeService =employeeService;
+        this.employeeService = employeeService;
 
     }
 
     @GetMapping
-    public String getAllOrders(Model model) {
+    public String getAllEmployees(Model model) {
         List<Employee> employees = employeeService.findAll();
         model.addAttribute("employees", employees);
         return "admin/employees";
+
     }
+
+    @PutMapping(value = "/{id}")
+    public String updateEmployee(@ModelAttribute Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/admin/employees";
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String removeEmployee(@PathVariable long id) {
+        Employee employee = employeeService.findOne(id);
+        employeeService.delete(employee);
+        return "redirect:/admin/employees";
+    }
+
+    @PostMapping
+    public String save(@ModelAttribute Employee employee){
+        employeeService.save(employee);
+        return "redirect:/admin/employess";
+    }
+
+    @GetMapping(value = "/createEmployee")
+    public String createEmployee(Model model){
+        model.addAttribute("employee", new Employee());
+        return "admin/employee";
+    }
+
 
 }
 
