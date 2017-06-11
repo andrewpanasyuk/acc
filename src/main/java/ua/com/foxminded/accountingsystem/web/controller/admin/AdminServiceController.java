d2f3dc8881c9fccee8e5ac0ec96bbc33621ec03c
VitaliyVoronov <vitalik.voronov@gmail.com>
2017-06-11 11:02:21 +0300

    change post to delete method

--- src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminServiceController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminServiceController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +41,7 @@ public class AdminServiceController {
         return "admin/services";
     }
 
-    @PostMapping("/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public String delete(@PathVariable long id) {
         log.debug("Remove service with id: " + id);
         if (id <= 0) {
