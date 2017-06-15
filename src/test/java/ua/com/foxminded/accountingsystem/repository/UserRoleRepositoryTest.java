1d9f76e6a82cb1329e28f924ae4b13df88c1b5d7
Maria Komar <maria.komar@gmail.com>
2017-06-15 12:11:14 +0300

    create constants for test values

--- src/test/java/ua/com/foxminded/accountingsystem/repository/UserRoleRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/UserRoleRepositoryTest.java
@@ -12,21 +12,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class UserRoleRepositoryTest extends AbstractRepositoryTest{
+    private static final String TEST_ROLE = "TEST";
+    private static final String UNKNOWN_ROLE = "unknown";
+
     @Autowired
     private UserRoleRepository userRoleRepository;
 
     @Test
     public void ifThereIsNoRolesWithSuchNameNullIsReturned() {
-        assertThat(userRoleRepository.findByRole("unknown"), is(nullValue()));
+        assertThat(userRoleRepository.findByRole(UNKNOWN_ROLE), is(nullValue()));
     }
 
     @Test
     public void ifRoleFoundItIsReturned() {
-        int id = addRoleToDatabase("TEST");
+        int id = addRoleToDatabase(TEST_ROLE);
         UserRole role = new UserRole();
-        role.setRole("TEST");
+        role.setRole(TEST_ROLE);
         role.setId(id);
-        assertEquals(userRoleRepository.findByRole("TEST"), role);
+        assertEquals(userRoleRepository.findByRole(TEST_ROLE), role);
     }
 
     private int addRoleToDatabase(String role) {
