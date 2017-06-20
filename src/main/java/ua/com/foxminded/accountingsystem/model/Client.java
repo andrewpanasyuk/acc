5cf42b1a3e259b1f9bbf2379bb5df674ec3cd019
Maria Komar <maria.komar@gmail.com>
2017-06-20 12:53:55 +0300

    add fields and mapping to Client and related classes

--- src/main/java/ua/com/foxminded/accountingsystem/model/Client.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/Client.java
@@ -8,18 +8,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
 @Table(name = "client")
-public class Client {
+public class Client implements Serializable{
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_sequence")
     @SequenceGenerator(name = "client_sequence", sequenceName = "client_sequence", initialValue = 50)
-    private long id;
+    private Long id;
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
@@ -30,12 +33,28 @@ public class Client {
     @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
 
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ClientFieldValue> extraFields;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL, optional = false, fetch = FetchType.LAZY)
+    private PersonalAccount personalAccount;
+
     public List<Order> getOrders() {
         return orders;
     }
 
     public void setOrders(List<Order> orders) {
         this.orders = orders;
+    }
+
+    public void addOrder(Order order){
+        orders.add(order);
+        order.setClient(this);
+    }
+
+    public void removeOrder(Order order){
+        orders.remove(order);
+        order.setClient(null);
     }
 
     public long getId() {
@@ -60,6 +79,48 @@ public class Client {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public List<ClientFieldValue> getExtraFields() {
+        return extraFields;
+    }
+
+    public void setExtraFields(List<ClientFieldValue> extraFields) {
+        this.extraFields = extraFields;
+    }
+
+    public void addClientFieldValue(ClientFieldValue clientFieldValue){
+        extraFields.add(clientFieldValue);
+        clientFieldValue.setClient(this);
+    }
+
+    public void removeClientFieldValue(ClientFieldValue clientFieldValue){
+        extraFields.remove(clientFieldValue);
+        clientFieldValue.setClient(null);
+    }
+
+    public PersonalAccount getPersonalAccount() {
+        return personalAccount;
+    }
+
+    public void setPersonalAccount(PersonalAccount personalAccount) {
+        this.personalAccount = personalAccount;
+        personalAccount.setClient(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        if (id != null && !id.equals(client.id)) return false;
+        if (!firstName.equals(client.firstName)) return false;
+        return lastName.equals(client.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
     }
 
     @Override
