59dc5b487d3d87b53930635a3f2e8379d3b18a5c
Maria Komar <maria.komar@gmail.com>
2017-06-20 12:53:55 +0300

    add fields and mapping to Client and related classes

--- src/main/java/ua/com/foxminded/accountingsystem/model/PersonalAccount.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/PersonalAccount.java
@@ -1,5 +1,40 @@
 package ua.com.foxminded.accountingsystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "personal_account")
 public class PersonalAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "personal_account_sequence")
+    @SequenceGenerator(name = "client_sequence", sequenceName = "personal_account_sequence", initialValue = 50)
+    private long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Client client;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
