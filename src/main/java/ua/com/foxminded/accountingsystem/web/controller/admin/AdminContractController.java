32d8545646700224441bba89f741a99205545558
Dmytro Kushnir <kushnir.do@gmail.com>
2017-06-23 23:14:29 +0300

    1. Add price and employeeRate on view. 2. Fields validation.

--- src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminContractController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminContractController.java
@@ -3,6 +3,7 @@ package ua.com.foxminded.accountingsystem.web.controller.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,14 +11,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ua.com.foxminded.accountingsystem.model.CloseType;
 import ua.com.foxminded.accountingsystem.model.Contract;
+import ua.com.foxminded.accountingsystem.model.Currency;
+import ua.com.foxminded.accountingsystem.model.Money;
 import ua.com.foxminded.accountingsystem.model.Order;
-import ua.com.foxminded.accountingsystem.model.PaymentType;
 import ua.com.foxminded.accountingsystem.service.ContractService;
 import ua.com.foxminded.accountingsystem.service.EmployeeService;
 import ua.com.foxminded.accountingsystem.service.OrderService;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 
 /**
@@ -62,7 +64,11 @@ public class AdminContractController {
     }
 
     @PostMapping()
-    public String save(@ModelAttribute Contract contract){
+    public String save(@Valid Contract contract, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("employees", employeeService.findAll());
+            return "admin/contract";
+        }
         contractService.save(contract);
         return "redirect:/admin/contracts";
     }
@@ -71,8 +77,24 @@ public class AdminContractController {
     public String newContract(@RequestParam long orderId, Model model){
         Contract contract = new Contract();
         Order order = orderService.findOne(orderId);
+
+        Money employeeRate = new Money();
+        employeeRate.setPrice(order.getService().getEmployeeRate().getPrice());
+        employeeRate.setCurrency(Currency.UAH);
+
+        Money price = new Money();
+        for (Money curPrice: order.getService().getPrices()) {
+            if (curPrice.getCurrency() == Currency.UAH){
+                price.setPrice(curPrice.getPrice());
+            }
+        }
+        price.setCurrency(Currency.UAH);
+
         contract.setOrder(order);
         contract.setContractDate(LocalDate.now());
+        contract.setEmployeeRate(employeeRate);
+        contract.setPrice(price);
+
         model
             .addAttribute("contract", contract)
             .addAttribute("employees", employeeService.findAll());
