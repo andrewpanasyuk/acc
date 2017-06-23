88970e662fa590a82fb7c737e87a5c9f4ab5148b
VitaliyVoronov <vitalik.voronov@gmail.com>
2017-06-23 17:14:48 +0300

    add test for serviceService

--- src/test/java/ua/com/foxminded/accountingsystem/repository/ServiceRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/ServiceRepositoryTest.java
@@ -1,68 +1,50 @@
 package ua.com.foxminded.accountingsystem.repository;
 
-import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.dataset.ExpectedDataSet;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.annotation.Commit;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import ua.com.foxminded.accountingsystem.model.Currency;
 import ua.com.foxminded.accountingsystem.model.Money;
 import ua.com.foxminded.accountingsystem.model.Service;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 
-public class ServiceRepositoryTest extends AbstractRepositoryTest<ServiceRepository> {
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ServiceRepositoryTest extends AbstractRepositoryTest {
 
-    private static Service service;
+    @Autowired
+    private ServiceRepository repository;
 
-    @BeforeClass
-    public static void init(){
+    Service service;
+
+    @Before
+    public void init(){
         service = new Service();
-        service.setName("java test");
-        service.getEmployeeRate().setCurrency(Currency.UAH);
+        service.setName("test");
+        service.getEmployeeRate().setCurrency(Currency.EUR);
         Money money = new Money();
-        money.setCurrency(Currency.UAH);
+        money.setCurrency(Currency.EUR);
         service.getPrices().add(money);
-        service.setDescription("java description");
+        service.setDescription("test description");
     }
 
     @Test
-    @Commit
-    @DataSet(value = "services/empty.xml", cleanBefore = true)
-    @ExpectedDataSet("services/expected-services.xml")
-    public void addService() {
-        repository.save(service);
-    }
-
-    @Test
-    @Commit
-    @DataSet(value = "services/stored-services.xml", cleanAfter = true)
-    public void testFindAllServices() {
+    public void testFindAll(){
         assertEquals(2, repository.findAll().size());
     }
 
     @Test
-    @Commit
-    @DataSet(value = "services/stored-services.xml", cleanAfter = true)
-    public void testFindOneServiceById() {
-        assertEquals("java test", repository.findOne(1L).getName());
+    public void testFindOne(){
+        assertEquals("java", repository.findOne(1L).getName());
     }
 
     @Test
-    @Commit
-    @DataSet(value = "services/stored-services.xml", cleanAfter = true)
-    public void ifServiceNotFoundByIdReturnNull() {
-        assertThat(repository.findOne(10L), nullValue());
+    public void testSave(){
+        repository.save(service);
+        assertEquals(3, repository.findAll().size());
     }
-
-    @Test
-    @Commit
-    @DataSet(value = "services/stored-services.xml")
-    @ExpectedDataSet("services/expected-services.xml")
-    public void deleteServiceById() {
-        repository.delete(2L);
-    }
-
 }
