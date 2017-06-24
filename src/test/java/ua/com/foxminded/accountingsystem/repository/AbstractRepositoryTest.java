988b85c76dd0c244889318d7cb7f285fe36c1315
alteran <e.smorodskiy@gmail.com>
2017-06-24 12:41:24 +0300

    db import fix

--- src/test/java/ua/com/foxminded/accountingsystem/repository/AbstractRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/AbstractRepositoryTest.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
 public abstract class AbstractRepositoryTest<T> {
 
     @Qualifier("dataSource")
