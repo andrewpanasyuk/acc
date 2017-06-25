94fced2f157aa73f85ab9e273d3ce84740136b41
VitaliyVoronov <vitalik.voronov@gmail.com>
2017-06-25 16:25:12 +0300

    service controller tests

--- src/test/java/ua/com/foxminded/accountingsystem/web/ServiceControllerTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/web/ServiceControllerTest.java
@@ -1,32 +1,36 @@
 package ua.com.foxminded.accountingsystem.web;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
 import ua.com.foxminded.accountingsystem.model.Service;
 import ua.com.foxminded.accountingsystem.service.ServiceService;
 import ua.com.foxminded.accountingsystem.web.controller.admin.AdminServiceController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ServiceControllerTest {
 
     @Mock
@@ -36,49 +40,47 @@ public class ServiceControllerTest {
 
     private MockMvc mockMvc;
 
+    private static Service service1;
+    private static Service service2;
+    private static List<Service> services;
+
+    @BeforeClass
+    public static void initClass(){
+        service1 = new Service();
+        service1.setName("test1");
+        service1.setDescription("test1 description");
+
+        service2 = new Service();
+        service2.setName("test2");
+        service2.setDescription("test2 description");
+
+        services = new ArrayList<>();
+        services.add(service1);
+        services.add(service2);
+    }
+
     @Before
     public void init() {
         controller = new AdminServiceController(serviceService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        Service service1 = new Service();
-        service1.setName("test1");
-        Service service2 = new Service();
-        service2.setName("test2");
+
         when(serviceService.findAll()).thenReturn(Arrays.asList(service1, service2));
         when(serviceService.findOne(1L)).thenReturn(service1);
 
     }
 
     @Test
-    public void requestForServicesPageIsSuccessfullyProcessed() throws Exception {
-        this.mockMvc.perform(get("/admin/services")
-            .accept(MediaType.parseMediaType("text/html;charset=UTF-8")))
-            .andExpect(status().isOk())
-            .andExpect(forwardedUrl("admin/services"));
+    public void servicesAddedToModel() {
+        Model model = new ExtendedModelMap();
+        assertThat(controller.getAllServices(model), equalTo("admin/services"));
+        assertThat(model.asMap(), hasEntry("services", services));
     }
 
     @Test
-    public void testForListServicesOnPage() throws Exception {
-        this.mockMvc.perform(get("/admin/services")
-            .accept(MediaType.parseMediaType("text/html;charset=UTF-8")))
-            .andExpect(status().isOk())
-            .andExpect(model().attribute("services", hasSize(2)));
-    }
-
-    public void requestForServicePageIsSuccessfullyProcessed() throws Exception {
-        this.mockMvc.perform(get("/admin/services/1")
-            .accept(MediaType.parseMediaType("text/html;charset=UTF-8")))
-            .andExpect(status().isOk())
-            .andExpect(forwardedUrl("admin/service"));
-    }
-
-    @Test
-    public void testForServicePageWithOneService() throws Exception {
-        this.mockMvc.perform(get("/admin/services/1")
-            .accept(MediaType.parseMediaType("text/html;charset=UTF-8")))
-            .andExpect(status().isOk())
-            .andExpect(model().attribute("service", notNullValue()
-            ));
+    public void oneServiceAddedToModel() {
+        Model model = new ExtendedModelMap();
+        assertThat(controller.getOneService(1, model), equalTo("admin/service"));
+        assertThat(model.asMap(), hasEntry("service", service1));
     }
 
 }
