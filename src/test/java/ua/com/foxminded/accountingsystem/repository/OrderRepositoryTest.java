63c7c591c7ecd6692b1dc69da04f4514de28469d
Andrew <avpk77@gmail.com>
2017-06-26 23:11:40 +0300

    added @ExpectedDataSet for testing "del"

--- src/test/java/ua/com/foxminded/accountingsystem/repository/OrderRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/OrderRepositoryTest.java
@@ -15,6 +15,7 @@ import ua.com.foxminded.accountingsystem.model.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -69,8 +70,6 @@ public class OrderRepositoryTest extends AbstractRepositoryTest<OrderRepository>
     @DataSet(value = "orders/stored-orders.xml", disableConstraints = true)
     public void findAllOrderTest() {
         assertEquals(2, repository.findAll().size());
-        assertTrue(repository.findOne(1L).equals(order));
-        assertTrue(repository.findOne(2L).equals(order_1));
     }
 
     @Test
@@ -80,11 +79,10 @@ public class OrderRepositoryTest extends AbstractRepositoryTest<OrderRepository>
     }
 
     @Test
+    @Commit
     @DataSet(value = "orders/stored-orders.xml", disableConstraints = true)
+    @ExpectedDataSet(value = "orders/expected-orders.xml")
     public void deleteOrderByIdTest() {
-        assertTrue(!repository.findOne(2L).equals(null));
         repository.delete(2L);
-        assertNull(repository.findOne(2L));
-
     }
 }
