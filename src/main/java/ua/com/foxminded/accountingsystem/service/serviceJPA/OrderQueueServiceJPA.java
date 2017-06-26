633217b6ba9a3715c55998f40d72d5ed50840915
Andrew <avpk77@gmail.com>
2017-06-26 22:25:15 +0300

    start work with OrderQueue

--- src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/OrderQueueServiceJPA.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/OrderQueueServiceJPA.java
@@ -2,14 +2,10 @@ package ua.com.foxminded.accountingsystem.service.serviceJPA;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.com.foxminded.accountingsystem.model.Order;
 import ua.com.foxminded.accountingsystem.model.OrderQueue;
-import ua.com.foxminded.accountingsystem.model.OrderStatus;
-import ua.com.foxminded.accountingsystem.model.Priority;
 import ua.com.foxminded.accountingsystem.repository.OrderQueueRepository;
 import ua.com.foxminded.accountingsystem.service.OrderQueueService;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -40,23 +36,5 @@ public class OrderQueueServiceJPA implements OrderQueueService {
     @Override
     public List<OrderQueue> findAll() {
         return orderQueueRepository.findAll();
-    }
-
-    @Override
-    public OrderQueue findByOrderId(Long id) {
-        return orderQueueRepository.findByOrderId(id);
-    }
-
-    public OrderQueue create(Order order) {
-        OrderQueue orderQueue = new OrderQueue();
-        orderQueue.setQueuingDate(LocalDate.now());
-        if (order.getStatus().equals(OrderStatus.NEW)){
-            orderQueue.setPriority(Priority.NORMAL);
-        } else {
-            orderQueue.setPriority(Priority.HIGH);
-        }
-        order.setStatus(OrderStatus.WAITING);
-        orderQueue.setOrder(order);
-        return orderQueue;
     }
 }
