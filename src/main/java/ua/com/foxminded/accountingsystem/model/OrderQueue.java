117eac902787521627fb2c21ce1e4ec9d5933f4f
Andrew <avpk77@gmail.com>
2017-06-26 22:25:15 +0300

    start work with OrderQueue

---
+++ src/main/java/ua/com/foxminded/accountingsystem/model/OrderQueue.java
@@ -0,0 +1,11 @@
+package ua.com.foxminded.accountingsystem.model;
+
+import java.time.LocalDate;
+
+public class OrderQueue {
+    private Long id;
+    private LocalDate queueDate;
+    private Priority priority;
+
+
+}
