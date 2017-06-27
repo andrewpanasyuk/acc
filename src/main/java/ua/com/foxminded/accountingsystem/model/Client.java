8a087f383c27b170e76d1110332c41bdfed6b704
Dmitriy Tkachenko <smartphone000014@gmail.com>
2017-06-27 17:02:21 +0300

    feat_130

--- src/main/java/ua/com/foxminded/accountingsystem/model/Client.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/Client.java
@@ -1,6 +1,16 @@
 package ua.com.foxminded.accountingsystem.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.List;
 
