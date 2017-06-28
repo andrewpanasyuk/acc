f36fc600a4d50f5c02eef7274228e13ca569c74d
Andrew <avpk77@gmail.com>
2017-06-28 14:27:47 +0300

    Basic queue implementation

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -165,3 +165,6 @@ INSERT INTO contract(id, contract_date,  order_id, employee_id, payment_type, pr
     (3, '2017-06-10', 2, 2, 'PREPAY', 5, 2),
     (4, '2017-06-15', 5, 3, 'PREPAY', 5, 2),
     (5, '2017-06-20',  6, 2, 'POSTPAY', 5, 2);
+
+INSERT INTO queue (id, queuing_date, priority, order_id) VALUES
+    (1, '2017-01-01', 'NORMAL', 1);
