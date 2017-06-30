339f093139696be683ae5a84ac10e7272647ff91
Andrew <avpk77@gmail.com>
2017-06-30 14:54:36 +0300

    Basic queue implementation (CRUD)

--- src/main/java/ua/com/foxminded/accountingsystem/repository/OrderQueueRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/OrderQueueRepository.java
@@ -1,7 +1,11 @@
 package ua.com.foxminded.accountingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.accountingsystem.model.OrderQueue;
 
 public interface OrderQueueRepository extends JpaRepository<OrderQueue, Long> {
+
+    @Query("SELECT queue FROM OrderQueue queue WHERE order_id = ?1")
+    OrderQueue findQueueByOrderId(Long id);
 }
