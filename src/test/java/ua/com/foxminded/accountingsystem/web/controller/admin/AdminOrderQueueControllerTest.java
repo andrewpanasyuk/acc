fa08793f6cab95e89d935c27722bbd1fd3f2471d
Andrew <avpk77@gmail.com>
2017-06-30 14:54:36 +0300

    Basic queue implementation (CRUD)

--- src/test/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminOrderQueueControllerTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminOrderQueueControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import ua.com.foxminded.accountingsystem.service.OrderQueueService;
+import ua.com.foxminded.accountingsystem.service.OrderService;
 
 import java.util.Arrays;
 
@@ -24,13 +25,16 @@ public class AdminOrderQueueControllerTest {
     @Mock
     private OrderQueueService orderQueueService;
 
+    @Mock
+    private OrderService orderService;
+
     private MockMvc mockMvc;
 
     private AdminOrderQueueController controller;
 
     @Before
     public void init() {
-        controller = new AdminOrderQueueController(orderQueueService);
+        controller = new AdminOrderQueueController(orderQueueService, orderService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
