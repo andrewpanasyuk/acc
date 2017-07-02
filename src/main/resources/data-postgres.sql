5316d052f8ea8fc72302ff467640384823e83177
Andrew <avpk77@gmail.com>
2017-07-02 16:36:45 +0300

    refactor for Merge 2

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -160,8 +160,11 @@ INSERT INTO invoice (id, creation_date, employee_payment, period_from, period_to
     (8, '2000-01-01', TRUE, '2000-01-01', '2000-01-01');
 
 INSERT INTO contract(id, contract_date,  order_id, employee_id, payment_type, price_id, employee_rate_id) VALUES
-    (1, '2017-06-01', 1, 1, 'PREPAYMENT', 5, 2),
-    (2, '2017-06-01', 1, 2, 'PREPAYMENT', 5, 2),
-    (3, '2017-06-10', 2, 2, 'PREPAYMENT', 5, 2),
-    (4, '2017-06-15', 5, 3, 'PREPAYMENT', 5, 2),
-    (5, '2017-06-20',  6, 2, 'DELAY', 5, 2);
+    (1, '2017-06-01', 1, 1, 'PREPAY', 5, 2),
+    (2, '2017-06-01', 1, 2, 'PREPAY', 5, 2),
+    (3, '2017-06-10', 2, 2, 'PREPAY', 5, 2),
+    (4, '2017-06-15', 5, 3, 'PREPAY', 5, 2),
+    (5, '2017-06-20',  6, 2, 'POSTPAY', 5, 2);
+
+INSERT INTO order_queue (id, queuing_date, priority, order_id) VALUES
+    (1, '2017-01-01', 'NORMAL', 1);
