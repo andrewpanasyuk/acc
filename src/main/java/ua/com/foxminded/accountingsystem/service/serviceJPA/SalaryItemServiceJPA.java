53276c1042e2ae861eff7f16fda8157cf2392ba6
Evgeniy Viktorov <yark1y@i.ua>
2017-07-06 18:39:59 +0300

    added SalaryItem, added logic for creating new ones for postpay

--- src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/SalaryItemServiceJPA.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/SalaryItemServiceJPA.java
@@ -9,11 +9,9 @@ import ua.com.foxminded.accountingsystem.service.SalaryItemService;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 
 public class SalaryItemServiceJPA implements SalaryItemService {
-
     private final SalaryItemRepository salaryItemRepository;
 
     @Autowired
@@ -27,27 +25,7 @@ public class SalaryItemServiceJPA implements SalaryItemService {
     }
 
     @Override
-    public void delete(SalaryItem salaryItem) {
-        salaryItemRepository.delete(salaryItem);
-    }
-
-    @Override
-    public SalaryItem save(SalaryItem salaryItem) {
-        return salaryItemRepository.save(salaryItem);
-    }
-
-    @Override
-    public SalaryItem findOne(Long id) {
-        return salaryItemRepository.findOne(id);
-    }
-
-    @Override
-    public List<SalaryItem> findAll() {
-        return salaryItemRepository.findAll();
-    }
-
-    @Override
-    public SalaryItem createSalaryItem(Invoice invoice) {
+    public SalaryItem createPostpaySalaryItem(Invoice invoice) {
         SalaryItem salaryItem = new SalaryItem();
         salaryItem.setInvoice(invoice);
         salaryItem.setEmployee(invoice.getContract().getEmployee());
@@ -62,7 +40,7 @@ public class SalaryItemServiceJPA implements SalaryItemService {
     }
 
     @Override
-    public SalaryItem createPretermSalaryItem(Invoice invoice, LocalDate closureDate) {
+    public SalaryItem createPostpayPretermSalaryItem(Invoice invoice, LocalDate closureDate) {
         SalaryItem salaryItem = new SalaryItem();
         long daysInPeriod = ChronoUnit.DAYS.between(invoice.getPaymentPeriodFrom(), invoice.getPaymentPeriodTo());
         long salaryItemPeriod = ChronoUnit.DAYS.between(invoice.getPaymentPeriodFrom(), closureDate);
