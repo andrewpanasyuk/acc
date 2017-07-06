f860563699a8cad1ea46673f9b915cee9bd42de1
Evgeniy Viktorov <yark1y@i.ua>
2017-07-06 18:39:59 +0300

    added SalaryItem, added logic for creating new ones for postpay

--- src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
@@ -1,25 +1,15 @@
 package ua.com.foxminded.accountingsystem.service;
 
 import ua.com.foxminded.accountingsystem.model.Invoice;
-import ua.com.foxminded.accountingsystem.model.Order;
 import ua.com.foxminded.accountingsystem.model.SalaryItem;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface SalaryItemService {
 
-    void delete(SalaryItem salaryItem);
+    SalaryItem createPostpaySalaryItem(Invoice invoice);
 
-    SalaryItem save(SalaryItem salaryItem);
-
-    SalaryItem findOne(Long id);
-
-    List<SalaryItem> findAll();
-
-    SalaryItem createSalaryItem(Invoice invoice);
-
-    SalaryItem createPretermSalaryItem(Invoice invoice, LocalDate closureDate);
+    SalaryItem createPostpayPretermSalaryItem(Invoice invoice, LocalDate closureDate);
 }
 
 
