2d97c9fe950ad401e852e1621c2b428567a49125
Maria Komar <maria.komar@gmail.com>
2017-07-07 21:48:43 +0300

    add repository method for finding contracts for payment

--- src/main/java/ua/com/foxminded/accountingsystem/repository/ContractRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/ContractRepository.java
@@ -1,7 +1,19 @@
 package ua.com.foxminded.accountingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.accountingsystem.model.Contract;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+
+    @Query("SELECT c FROM Contract as c WHERE c.closeType is NULL " +
+        "AND function('DAY', c.paymentDate) = ?1 " +
+        "AND c.id NOT IN(" +
+        "SELECT processed FROM Contract as processed join processed.invoices as i " +
+        "WHERE i.creationDate = ?2 )")
+    List<Contract> findContractsForPayment(int payDay, LocalDate today);
 }
