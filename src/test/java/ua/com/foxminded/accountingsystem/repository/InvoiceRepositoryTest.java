8e8a2bc48dc8eeebf1c36036da9e97564218dcfd
Evgeniy Viktorov <yark1y@i.ua>
2017-07-09 12:58:05 +0300

    added findAll test

--- src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
@@ -12,44 +12,38 @@ import ua.com.foxminded.accountingsystem.model.Money;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+
 public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceRepository> {
 
     private static Invoice invoice_1;
-    private static Invoice invoice_2;
+//    private static Invoice invoice_2;
 
     @BeforeClass
     public static void init() {
-        Money moneyOfContractOne = new Money();
-        moneyOfContractOne.setId(1L);
-        moneyOfContractOne.setCurrency(Currency.EUR);
-        moneyOfContractOne.setPrice(1500);
+        Money money_1 = new Money();
+        money_1.setId(50L);
+        money_1.setCurrency(Currency.EUR);
+        money_1.setPrice(1500);
         Money moneyOfContractTwo = new Money();
         moneyOfContractTwo.setId(2L);
         moneyOfContractTwo.setCurrency(Currency.EUR);
         moneyOfContractTwo.setPrice(2000);
 
-        Contract contract1 = new Contract();
-        contract1.setId(1L);
-        contract1.setPaymentDate(LocalDate.of(2010, 1, 1));
-
-        Contract contract2 = new Contract();
-        contract2.setId(2L);
-        contract2.setPaymentDate(LocalDate.of(2016, 1, 1));
+        Contract contract_1 = new Contract();
+        contract_1.setId(50L);
+        contract_1.setPaymentDate(LocalDate.of(2010, 1, 1));
 
         invoice_1 = new Invoice();
-        invoice_1.setContract(contract1);
-        invoice_1.setCreationDate(contract1.getPaymentDate());
-        invoice_1.setPrice(moneyOfContractOne);
-        invoice_1.setPaymentPeriodFrom(contract1.getPaymentDate());
-        invoice_1.setPaymentPeriodTo(contract1.getPaymentDate().plusMonths(1L));
+        invoice_1.setId(50L);
+        invoice_1.setContract(contract_1);
+        invoice_1.setPrice(money_1);
+        invoice_1.setCreationDate(contract_1.getPaymentDate());
+        invoice_1.setPaymentPeriodFrom(contract_1.getPaymentDate());
+        invoice_1.setPaymentPeriodTo(contract_1.getPaymentDate().plusMonths(1L));
+        invoice_1.setEmployeePaid(true);
 
-        invoice_2 = new Invoice();
-        invoice_2.setContract(contract1);
-        invoice_2.setCreationDate(contract1.getPaymentDate().plusMonths(1L));
-        invoice_2.setPrice(moneyOfContractOne);
-        invoice_2.setPaymentPeriodFrom(contract1.getPaymentDate().plusMonths(1L));
-        invoice_2.setPaymentPeriodTo(contract1.getPaymentDate().plusMonths(2L));
-        invoice_2.setEmployeePaid(true);
     }
 
 
@@ -57,9 +51,13 @@ public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceReposit
     @Commit
     @DataSet(value = "invoices/empty.xml", disableConstraints = true, cleanBefore = true)
     @ExpectedDataSet(value = "invoices/expected-invoices.xml")
-    public void twoInvoicesAreAdded() {
+    public void OneInvoiceIsAdded() {
         repository.save(invoice_1);
-        repository.save(invoice_2);
     }
 
+    @Test
+    @DataSet("invoices/stored-invoices.xml")
+    public void invoiceExistsInRepo() {
+        assertThat(repository.findAll(), hasItems(invoice_1));
+    }
 }
