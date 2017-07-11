2c234f2271a7e0e7e9208f9db48006b4e340442a
Evgeniy Viktorov <yark1y@i.ua>
2017-07-11 15:39:59 +0300

    added basic crud to SalaryItem

--- src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
@@ -1,15 +1,25 @@
 package ua.com.foxminded.accountingsystem.service;
 
 import ua.com.foxminded.accountingsystem.model.Invoice;
+import ua.com.foxminded.accountingsystem.model.Order;
 import ua.com.foxminded.accountingsystem.model.SalaryItem;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface SalaryItemService {
 
-    SalaryItem createPostpaySalaryItem(Invoice invoice);
+    void delete(SalaryItem salaryItem);
 
-    SalaryItem createPostpayPretermSalaryItem(Invoice invoice, LocalDate closureDate);
+    SalaryItem save(SalaryItem salaryItem);
+
+    SalaryItem findOne(Long id);
+
+    List<SalaryItem> findAll();
+
+    SalaryItem createSalaryItem(Invoice invoice);
+
+    SalaryItem createPretermSalaryItem(Invoice invoice, LocalDate closureDate);
 }
 
 
