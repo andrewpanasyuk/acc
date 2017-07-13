8ea70c60ad8a3552d5a393f92050ab4847003c51
Evgeniy Viktorov <yark1y@i.ua>
2017-07-13 14:23:22 +0300

    added contract repo is invoice tests to fix em

--- src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
@@ -4,6 +4,7 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
 import ua.com.foxminded.accountingsystem.model.Contract;
 import ua.com.foxminded.accountingsystem.model.Currency;
@@ -22,21 +23,25 @@ import static org.hamcrest.Matchers.nullValue;
 public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceRepository> {
 
     private static Invoice invoice_1;
+    private static Contract contract_1;
+
+    @Autowired
+    private ContractRepository contractRepository;
 
     @BeforeClass
     public static void init() {
         Money money_1 = new Money();
-        money_1.setId(50L);
+        money_1.setId(1L);
         money_1.setCurrency(Currency.EUR);
         money_1.setPrice(1500);
 
-        Contract contract_1 = new Contract();
+        contract_1 = new Contract();
         contract_1.setId(50L);
         contract_1.setCreatedBy("system");
         contract_1.setCreatedDate(LocalDateTime.now());
 
         invoice_1 = new Invoice();
-        invoice_1.setId(50L);
+        invoice_1.setId(1L);
         invoice_1.setContract(contract_1);
         invoice_1.setPrice(money_1);
         invoice_1.setCreationDate(LocalDate.of(2010, 1, 1));
@@ -45,15 +50,14 @@ public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceReposit
         invoice_1.setEmployeePaid(true);
         invoice_1.setCreatedBy("system");
         invoice_1.setCreatedDate(LocalDateTime.now());
-
     }
-
 
     @Test
     @Commit
     @DataSet(value = "invoices/empty.xml", disableConstraints = true, cleanBefore = true)
-    @ExpectedDataSet(value = "invoices/expected-invoices.xml", ignoreCols = {"created_by", "created_date"})
+    @ExpectedDataSet(value = "invoices/expected-invoices.xml", ignoreCols = {"id", "contract_id", "closing_description", "close_date", "close_type", "contract_date", "order_id", "price_id", "payment_date", "payment_type", "open_date", "employee_id", "employee_rate_id", "close_date", "created_by", "created_date"})
     public void invoiceIsAdded() {
+        contractRepository.save(contract_1);
         repository.save(invoice_1);
     }
 
