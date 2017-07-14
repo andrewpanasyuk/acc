1ed8e0c6fc395b4956c6475b6de927889eafac36
Maria Komar <maria.komar@gmail.com>
2017-07-14 11:18:10 +0300

    add finding unaccounted invoices for date range

--- src/main/java/ua/com/foxminded/accountingsystem/repository/SalaryItemRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/SalaryItemRepository.java
@@ -1,7 +1,14 @@
 package ua.com.foxminded.accountingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.accountingsystem.model.SalaryItem;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SalaryItemRepository extends JpaRepository<SalaryItem, Long> {
+
+    @Query("select distinct s from SalaryItem s where s.accounted = false and s.dateFrom >= ?1 and s.dateTo <= ?2")
+    List<SalaryItem> findByDatePeriod(LocalDate dateFrom, LocalDate dateTo);
 }
