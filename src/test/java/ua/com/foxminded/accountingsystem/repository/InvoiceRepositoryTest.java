c0b75c255023bdc49058c9e9ee890d17e728871e
Evgeniy Viktorov <yark1y@i.ua>
2017-07-22 10:28:26 +0300

    fixed invoiceIsAdded()

--- src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
+++ src/test/java/ua/com/foxminded/accountingsystem/repository/InvoiceRepositoryTest.java
@@ -4,7 +4,6 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
 import ua.com.foxminded.accountingsystem.model.Contract;
 import ua.com.foxminded.accountingsystem.model.Currency;
@@ -16,40 +15,35 @@ import java.time.LocalDateTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
 public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceRepository> {
 
-    private static Invoice invoice_1;
-    private static Contract contract_1;
-
-    @Autowired
-    private ContractRepository contractRepository;
+    private static Invoice invoice;
+    private static Contract contract;
 
     @BeforeClass
     public static void init() {
-        Money money_1 = new Money();
-        money_1.setId(1L);
-        money_1.setCurrency(Currency.EUR);
-        money_1.setPrice(1500);
+        Money money = new Money();
+        money.setId(1L);
+        money.setCurrency(Currency.EUR);
+        money.setPrice(1500);
 
-        contract_1 = new Contract();
-        contract_1.setId(50L);
-        contract_1.setCreatedBy("system");
-        contract_1.setCreatedDate(LocalDateTime.now());
+        contract = new Contract();
+        contract.setCreatedBy("system");
+        contract.setCreatedDate(LocalDateTime.now());
 
-        invoice_1 = new Invoice();
-        invoice_1.setId(1L);
-        invoice_1.setContract(contract_1);
-        invoice_1.setPrice(money_1);
-        invoice_1.setCreationDate(LocalDate.of(2010, 1, 1));
-        invoice_1.setPaymentPeriodFrom(LocalDate.of(2010, 1, 1));
-        invoice_1.setPaymentPeriodTo(LocalDate.of(2010, 1, 1).plusMonths(1L));
-        invoice_1.setEmployeePaid(true);
-        invoice_1.setCreatedBy("system");
-        invoice_1.setCreatedDate(LocalDateTime.now());
+        invoice = new Invoice();
+        invoice.setId(1L);
+        invoice.setContract(contract);
+        invoice.setPrice(money);
+        invoice.setCreationDate(LocalDate.of(2010, 1, 1));
+        invoice.setPaymentPeriodFrom(LocalDate.of(2010, 1, 1));
+        invoice.setPaymentPeriodTo(LocalDate.of(2010, 1, 1).plusMonths(1L));
+        invoice.setEmployeePaid(true);
+        invoice.setCreatedBy("system");
+        invoice.setCreatedDate(LocalDateTime.now());
     }
 
     @Test
@@ -57,14 +51,8 @@ public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceReposit
     @DataSet(value = "invoices/empty.xml", disableConstraints = true, cleanBefore = true)
     @ExpectedDataSet(value = "invoices/expected-invoices.xml", ignoreCols = {"id", "contract_id", "price_id", "created_by", "created_date"})
     public void invoiceIsAdded() {
-        contractRepository.save(contract_1);
-        repository.save(invoice_1);
-    }
-
-    @Test
-    @DataSet("invoices/stored-invoices.xml")
-    public void invoiceExistsInRepo() {
-        assertThat(repository.findAll(), hasItems(invoice_1));
+        em.persist(contract);
+        repository.save(invoice);
     }
 
     @Test
@@ -84,6 +72,6 @@ public class InvoiceRepositoryTest extends AbstractRepositoryTest<InvoiceReposit
     @DataSet("invoices/expected-invoices.xml")
     @ExpectedDataSet("invoices/empty.xml")
     public void invoiceIsDeleted() {
-        repository.delete(invoice_1);
+        repository.delete(invoice);
     }
 }
