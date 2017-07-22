78018f281f07b7be4dab78e37ec0384ca117c29b
Maria Komar <maria.komar@gmail.com>
2017-07-22 19:57:21 +0300

    set allocationSize=1 for Employee/ClientFieldValue

--- src/main/java/ua/com/foxminded/accountingsystem/model/EmployeeFieldValue.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/EmployeeFieldValue.java
@@ -21,7 +21,8 @@ public class EmployeeFieldValue extends AbstractAuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_field_value_sequence")
-    @SequenceGenerator(name = "employee_field_value_sequence", sequenceName = "employee_field_value_sequence", initialValue = 50)
+    @SequenceGenerator(name = "employee_field_value_sequence", sequenceName = "employee_field_value_sequence",
+        initialValue = 50, allocationSize=1)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
