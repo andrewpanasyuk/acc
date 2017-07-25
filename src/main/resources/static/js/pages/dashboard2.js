710019d07b0c4a03f1b03bf277b1e01ddaaf10bb
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-07-25 10:23:56 +0300

    add ajax call and rest controller

--- src/main/resources/static/js/pages/dashboard2.js
+++ src/main/resources/static/js/pages/dashboard2.js
@@ -236,14 +236,14 @@ $(function () {
   //-----------------
   //- SPARKLINE BAR -
   //-----------------
-  $('.sparkbar').each(function () {
-    var $this = $(this);
-    $this.sparkline('html', {
-      type: 'bar',
-      height: $this.data('height') ? $this.data('height') : '30',
-      barColor: $this.data('color')
-    });
-  });
+  // $('.sparkbar').each(function () {
+  //   var $this = $(this);
+  //   $this.sparkline('html', {
+  //     type: 'bar',
+  //     height: $this.data('height') ? $this.data('height') : '30',
+  //     barColor: $this.data('color')
+  //   });
+  // });
 
   //-----------------
   //- SPARKLINE PIE -
