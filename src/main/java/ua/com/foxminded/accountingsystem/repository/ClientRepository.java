20e8a04e92e777f57123a62e38ad1280f18f581e
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-07-27 00:54:13 +0300

    prepare data for widget

--- src/main/java/ua/com/foxminded/accountingsystem/repository/ClientRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/ClientRepository.java
@@ -1,26 +1,21 @@
 package ua.com.foxminded.accountingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.accountingsystem.model.Client;
 import ua.com.foxminded.accountingsystem.model.OrderStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * Created by Andrew on 04.06.2017.
  */
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    long countClientByOrders_Status(OrderStatus status);
+    long countClientByOrdersStatus(OrderStatus status);
 
     long countClientByCreatedDateAfter(LocalDateTime date);
 
-    @Query("SELECT count(client) FROM Client client " +
-        "JOIN client.orders orders " +
-        "JOIN Contract contract")
-    long countClientByOrdersFrozenForLastMonth();
+    long countClientByOrdersStatusAndOrdersCloseDateAfter(OrderStatus status, LocalDate closeDate);
 
 }
