58c33d578d6e321098e7e5b4b05e00dae0127e37
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-07-26 17:11:58 +0300

    add ClientStatisticsDto

--- src/main/java/ua/com/foxminded/accountingsystem/repository/ClientRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/ClientRepository.java
@@ -2,10 +2,18 @@ package ua.com.foxminded.accountingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.foxminded.accountingsystem.model.Client;
+import ua.com.foxminded.accountingsystem.model.OrderStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Created by Andrew on 04.06.2017.
  */
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    long countClientByOrders_Status(OrderStatus status);
+
+    long countClientByCreatedByAfter(LocalDateTime date);
 }
--- src/main/java/ua/com/foxminded/accountingsystem/web/rest/StatisticsRestController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/rest/StatisticsRestController.java
@@ -2,25 +2,32 @@ package ua.com.foxminded.accountingsystem.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
+import ua.com.foxminded.accountingsystem.service.ClientService;
+import ua.com.foxminded.accountingsystem.service.dto.ClientStatisticsDto;
 
 /**
  * Created by andreb on 24.07.17.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/statistics")
 public class StatisticsRestController {
 
     private static final Logger log = LoggerFactory.getLogger(StatisticsRestController.class);
 
-    @GetMapping("/statistics")
-    public List<Integer> getStatictics() {
+    private final ClientService clientService;
+
+    @Autowired
+    public StatisticsRestController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/clients")
+    public ClientStatisticsDto getStudentStatistics() {
         log.debug("Get statistics");
-        return Arrays.asList(20, 5, 3, 8);
+        return clientService.getClientStatistics();
     }
 }
