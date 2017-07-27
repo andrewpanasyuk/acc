716a4b170a20ae0dfd28f08249c2c0a950fb312d
alteran <e.smorodskiy@gmail.com>
2017-07-27 13:43:37 +0300

    add name sorter

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $('#fixed-table-toolbar--buttons').prependTo('.fixed-table-toolbar');
 });
+
+function nameSorter(a, b) {
+    textFirst = a.slice(a.indexOf('>') + 1);
+    textSecond = b.slice(b.indexOf('>') + 1);
+    return textFirst.localeCompare(textSecond);
+}
