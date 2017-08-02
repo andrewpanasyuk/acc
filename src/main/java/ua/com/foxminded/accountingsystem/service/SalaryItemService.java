6b93e387a9fad82bfad63e07bf6d1fad0160a297
Evgeniy Viktorov <yark1y@i.ua>
2017-08-02 13:57:00 +0300

    broght back LocalDate @ createPretermSalaryItem logic for future uses

--- src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/SalaryItemService.java
@@ -3,6 +3,7 @@ package ua.com.foxminded.accountingsystem.service;
 import ua.com.foxminded.accountingsystem.model.Invoice;
 import ua.com.foxminded.accountingsystem.model.SalaryItem;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SalaryItemService {
@@ -15,9 +16,9 @@ public interface SalaryItemService {
 
     List<SalaryItem> findAll();
 
-    void createSalaryItem(Invoice invoice);
+    SalaryItem createSalaryItem(Invoice invoice);
 
-    void createPretermSalaryItem(Invoice invoice);
+    SalaryItem createPretermSalaryItem(Invoice invoice, LocalDate closureDate);
 }
 
 
