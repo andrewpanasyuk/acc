24b99fb2b56b972a21e6cb3e6fa45993f20a3937
Andrew <avpk77@gmail.com>
2017-08-07 17:29:50 +0300

    correct method(del 'if' loop)

--- src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminOrderQueueController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminOrderQueueController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.com.foxminded.accountingsystem.model.OrderQueue;
+import ua.com.foxminded.accountingsystem.model.OrderStatus;
 import ua.com.foxminded.accountingsystem.model.Service;
 import ua.com.foxminded.accountingsystem.service.OrderQueueService;
 
