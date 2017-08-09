7f223aff5148ea9de0efe997c6b991fc28bfb331
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-08-10 00:32:53 +0300

    refactor

--- src/main/java/ua/com/foxminded/accountingsystem/service/exception/NotEnoughMoneyWithdrawException.java
+++
@@ -1,7 +0,0 @@
-package ua.com.foxminded.accountingsystem.service.exception;
-
-public class NotEnoughMoneyWithdrawException extends RuntimeException {
-    public NotEnoughMoneyWithdrawException(String message) {
-        super(message);
-    }
-}
