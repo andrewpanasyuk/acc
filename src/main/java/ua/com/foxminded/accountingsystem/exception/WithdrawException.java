14a9cb5655a152767606cadc044f89255b8d1b83
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-08-09 19:12:43 +0300

    deleted comments

--- src/main/java/ua/com/foxminded/accountingsystem/exception/WithdrawException.java
+++ src/main/java/ua/com/foxminded/accountingsystem/exception/WithdrawException.java
@@ -1,8 +1,5 @@
 package ua.com.foxminded.accountingsystem.exception;
 
-/**
- * Created by andreb on 09.08.17.
- */
 public class WithdrawException extends RuntimeException {
     public WithdrawException(String message) {
         super(message);
--- src/main/java/ua/com/foxminded/accountingsystem/model/TransferType.java
+++ src/main/java/ua/com/foxminded/accountingsystem/model/TransferType.java
@@ -1,8 +1,5 @@
 package ua.com.foxminded.accountingsystem.model;
 
-/**
- * Created by andreb on 04.08.17.
- */
 public enum TransferType {
     INCOME, OUTCOME;
 }
