fe03eee9e93b095225cba82b4f94aec60afb99e5
Andrew <avpk77@gmail.com>
2017-08-11 14:12:36 +0300

    deleted bad words

--- src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminInvoiceController.java
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/admin/AdminInvoiceController.java
@@ -81,7 +81,6 @@ public class AdminInvoiceController {
 
     @PostMapping("/issue")
     public String issueInvoice(@ModelAttribute Invoice invoice) {
-        System.out.println(invoice);
         invoiceService.issueInvoice(invoice);
         return "redirect:/admin/invoices/issue";
     }
