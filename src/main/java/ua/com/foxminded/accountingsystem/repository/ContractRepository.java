81bc516012618799865458baab5e47ed662a6f25
Andrew <avpk77@gmail.com>
2017-08-14 09:57:20 +0300

    change 'NOT IN' on 'NOT EXISTS'

--- src/main/java/ua/com/foxminded/accountingsystem/repository/ContractRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/ContractRepository.java
@@ -12,9 +12,8 @@ public interface ContractRepository extends JpaRepository<Contract, Long> {
     @Query("SELECT c FROM Contract as c WHERE c.closeType is NULL " +
         "AND function('DAY', c.paymentDate) <= ?1 " +
         "AND c.paymentType is NOT ua.com.foxminded.accountingsystem.model.PaymentType.TRIAL " +
-        "AND c.id NOT IN(SELECT c FROM Contract c JOIN c.invoices as i " +
-        "LEFT JOIN i.payment as p WHERE p.id IS NULL)")
-
+        "AND NOT EXISTS(SELECT contract FROM Contract contract JOIN contract.invoices as i " +
+        "LEFT JOIN i.payment as p WHERE p.id IS NULL AND contract.id = c.id)")
     List<Contract> findContractsForInvoicesCreation(int payDay);
 
     List<Contract> findAllByDealOrderByContractDateDesc(Deal deal);
