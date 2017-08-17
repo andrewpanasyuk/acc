5f78908b6f5199a8e785367f10ec75b06bc9041e
Andrew <avpk77@gmail.com>
2017-08-17 17:25:42 +0300

    separate migration for dev and prod

--- src/main/java/ua/com/foxminded/accountingsystem/config/FlywayConfig.java
+++ src/main/java/ua/com/foxminded/accountingsystem/config/FlywayConfig.java
@@ -1,5 +1,6 @@
 package ua.com.foxminded.accountingsystem.config;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,25 @@ import org.springframework.context.annotation.Profile;
 public class FlywayConfig {
 
     @Bean
-    @Profile("dev")
+    @Profile("prod")
     public FlywayMigrationStrategy cleanMigrateStrategy() {
 
         return flyway -> {
             flyway.clean();
+            flyway.setLocations("db\\prod");
             flyway.migrate();
         };
     }
+
+    @Bean
+    @Profile("dev")
+    public FlywayMigrationStrategy migrateStrategy() {
+
+        return flyway -> {
+            flyway.clean();
+            flyway.setLocations("db\\dev");
+            flyway.migrate();
+        };
+    }
+
 }
