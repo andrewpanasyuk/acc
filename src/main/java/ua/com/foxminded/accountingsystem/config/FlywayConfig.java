b0b48371c3583137b4defb17208a77668a86099e
Andrew <avpk77@gmail.com>
2017-08-17 14:49:31 +0300

    next_step_2.1

--- src/main/java/ua/com/foxminded/accountingsystem/config/FlywayConfig.java
+++ src/main/java/ua/com/foxminded/accountingsystem/config/FlywayConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 public class FlywayConfig {
 
     @Bean
-    @Profile("prod")
+    @Profile("dev")
     public FlywayMigrationStrategy cleanMigrateStrategy() {
 
         return flyway -> {
