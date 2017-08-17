2215394e2d49b208369e3855fbbb6cc0be5e0779
Andrew <avpk77@gmail.com>
2017-08-17 12:47:43 +0300

    first_step

--- src/main/resources/db/migration/V2__fm_db_version.sql
+++ src/main/resources/db/migration/V2__fm_db_version.sql
@@ -4,12 +4,10 @@ INSERT INTO fm_users (username, password, enabled, email, created_by, created_da
     ('inactiveUser', '$2a$10$DLUI1caX.tFT6u/xCiQzluxo7UvHxYhTivcdU6SyV67Q90qZaJ9Qq', FALSE, 'inactiveuser@mail.com', 'system', '2017-07-11' ),
     ('superMentor', '2a$10$BvU/7BcF0DBrnpxlN9r4cuyABANhhzbPhCXeWLTOPwcAYXA6jbcYK', TRUE, 'superMentor@mail.com', 'system', '2017-07-11' ),
     ('mentor', '2a$10$BvU/7BcF0DBrnpxlN9r4cuyABANhhzbPhCXeWLTOPwcAYXA6jbcYK', TRUE, 'mentor@mail.com', 'system', '2017-07-11' );
-
 INSERT INTO user_role (id, role, created_by, created_date) VALUES
     (1, 'ADMIN', 'system', '2017-07-11'),
     (2, 'USER', 'system', '2017-07-11'),
     (3, 'MENTOR', 'system', '2017-07-11');
-
 INSERT INTO fm_users_user_role (username, role_id) VALUES
     ('admin', 1),
     ('admin', 2),
@@ -18,17 +16,14 @@ INSERT INTO fm_users_user_role (username, role_id) VALUES
     ('inactiveUser', 2),
     ('superMentor', 3),
     ('mentor', 3);
-
 INSERT INTO employee (id, first_name, last_name, max_clients, username, created_by, created_date) VALUES
     (1, 'Charles', 'Xavier', 20, 'superMentor', 'system', '2017-07-11'),
     (2, 'James', 'Howlett', 1, NULL, 'system', '2017-07-11'),
     (3, 'Scott', 'Summers', 3, NULL, 'system', '2017-07-11'),
     (4, 'Ororo', 'Munroe', 10, 'mentor', 'system', '2017-07-11');
-
 INSERT INTO employee_field (id, name, created_by, created_date) VALUES
     (1, 'skype', 'system', '2017-07-11'),
     (2, 'email', 'system', '2017-07-11');
-
 INSERT INTO employee_field_value (id, employee_id, employee_field_id, value, created_by, created_date) VALUES
     (1, 1, 1, 'ProfessorX', 'system', '2017-07-11'),
     (2, 1, 2, NULL, 'system', '2017-07-11'),
@@ -38,7 +33,6 @@ INSERT INTO employee_field_value (id, employee_id, employee_field_id, value, cre
     (6, 3, 2, 'cyclops@mail.com', 'system', '2017-07-11'),
     (7, 4, 1, 'Storm', 'system', '2017-07-11'),
     (8, 4, 2, 'lightning@mail.com', 'system', '2017-07-11');
-
 INSERT INTO money (id, currency, amount) VALUES
     (1, 'USD', 20),
     (2, 'EUR', 9999),
@@ -84,14 +78,12 @@ INSERT INTO money (id, currency, amount) VALUES
     (42, 'UAH', 4500),
     (43, 'UAH', 1000),
     (44, 'UAH', 1500);
-
 INSERT INTO consultancy (id, name, description, employee_rate_id, created_by, created_date) VALUES
     (1, 'Mentoring', 'Focuses on the knowledge and needs of clients. Training during free time. No lectures.', 9, 'system', '2017-07-11'),
     (2, 'Personal Mentor', 'Personal couching with Sergey Nemchinskiy', 10, 'system', '2017-07-11'),
     (3, 'Group Development', 'Interact with the team;
 Solve conflicts that inevitably arise during teamwork;
 Create a project from scratch.', 11, 'system', '2017-07-11');
-
 INSERT INTO consultancy_prices (consultancy_id, prices_id) VALUES
     (1, 12),
     (1, 13),
@@ -102,7 +94,6 @@ INSERT INTO consultancy_prices (consultancy_id, prices_id) VALUES
     (3, 18),
     (3, 19),
     (3, 20);
-
 INSERT INTO personal_account (id) VALUES
     (1),
     (2),
@@ -110,7 +101,6 @@ INSERT INTO personal_account (id) VALUES
     (4),
     (5),
     (6);
-
 INSERT INTO personal_account_money (personal_account_id, money_id) VALUES
     (1, 1),
     (2, 2),
@@ -121,7 +111,6 @@ INSERT INTO personal_account_money (personal_account_id, money_id) VALUES
     (4, 7),
     (5, 8),
     (6, 31);
-
 INSERT INTO client (id, first_name, last_name, personal_account_id, created_by, created_date) VALUES
     (1, 'John', 'Allerdyce', 1, 'admin', '2017-07-11'),
     (2, 'Raven', 'Darkhölme', 2, 'admin', '2017-07-11'),
@@ -129,11 +118,9 @@ INSERT INTO client (id, first_name, last_name, personal_account_id, created_by, 
     (4, 'Piotr', 'Rasputin', 4, 'admin', '2017-07-11'),
     (5, 'Kurt', 'Wagner', 5, 'admin', '2017-07-11'),
     (6, 'Marie', 'Rogue', 6, 'admin', '2017-07-11');
-
 INSERT INTO client_field (id, name, created_by, created_date) VALUES
     (1, 'skype', 'system', '2017-07-11'),
     (2, 'email', 'system', '2017-07-11');
-
 INSERT INTO client_field_value (id, client_id, client_field_id, value, created_by, created_date) VALUES
     (1, 1, 1, 'Pyro', 'system', '2017-07-11'),
     (2, 1, 2, 'fire@mail.com', 'system', '2017-07-11'),
@@ -147,8 +134,6 @@ INSERT INTO client_field_value (id, client_id, client_field_id, value, created_b
     (10, 5, 2, NULL, 'system', '2017-07-11'),
     (11, 6, 1, NULL, 'system', '2017-07-11'),
     (12, 6, 2, NULL, 'system', '2017-07-11');
-
-
 INSERT INTO deal (id, consultancy_id, status, client_id, open_date, close_date, created_by, created_date) VALUES
     (1, 3, 'NEW', 4, now() - interval '1 day', NULL, 'system', '2017-07-11'),
     (2, 1, 'WAITING', 1, now() - interval '3 day', NULL, 'system', '2017-07-11'),
@@ -161,8 +146,6 @@ INSERT INTO deal (id, consultancy_id, status, client_id, open_date, close_date, 
     (9, 2, 'FROZEN', 1, now() - interval '20 day', NULL, 'system', '2017-07-11'),
     (10, 1, 'FROZEN', 6, now() - interval '2 mon + 20 day', NULL, 'system', '2017-07-11'),
     (11, 3, 'ACTIVE', 6, now() - interval '8 day', NULL, 'system', '2017-07-11');
-
-
 INSERT INTO contract (id, deal_id, contract_date, employee_id, payment_type, price_id, employee_rate_id, payment_date, close_date, close_type, created_by, created_date)
 VALUES
     (1, 5, now() - interval '4 mon + 10 day', 1, 'TRIAL', 21, 22, NULL, now() - interval '4 mon + 1 day', 'COMPLETED', 'system', '2017-07-11'),
@@ -173,7 +156,6 @@ VALUES
     (6, 10, now() - interval '2 mon + 13 day', 2, 'PREPAY', 27, 28, now() - interval '2 mon + 10 day', now() - interval '1 mon', 'FROZEN', 'system', '2017-07-11'),
     (7, 11, now() - interval '8 day', 1, 'TRIAL', 29, 30, NULL, NULL, NULL, 'system', '2017-07-11'),
     (8, 11, now() - interval '1 day', 1, 'PREPAY', 29, 30, now() + interval '2 day', NULL, NULL, 'system', '2017-07-11');
-
 INSERT INTO invoice (id, contract_id, price_id, creation_date, employee_paid, period_from, period_to, created_by, created_date) VALUES
     (1, 2, 21, now() - interval '3 mon + 3 day', FALSE, now() - interval '4 mon', now() - interval '3 mon', 'system', '2017-07-11'),
     (2, 2, 21, now() - interval '2 mon + 3 day', FALSE, now() - interval '3 mon', now() - interval '2 mon', 'system', '2017-07-11'),
@@ -186,7 +168,6 @@ INSERT INTO invoice (id, contract_id, price_id, creation_date, employee_paid, pe
     (9, 6, 27, now() - interval '2 mon + 13 day', FALSE, now() - interval '2 mon + 10 day', now() - interval '1 mon + 10 day', 'system', '2017-07-11'),
     (10, 6, 27, now() - interval '1 mon + 13 day', FALSE, now() - interval '1 mon + 10 day', now() - interval '10 day', 'system', '2017-07-11'),
     (11, 8, 29, now() - interval '1 day', FALSE, now() + interval '2 day', now() + interval ' 1 mon 1 day', 'system', '2017-07-11');
-
 INSERT INTO payment (id, invoice_id, date_paid, sum_id, created_by, created_date) VALUES
     (1, 1, now() - interval '3 mon', 32, 'system', '2017-07-11'),
     (2, 2, now() - interval '2 mon', 33, 'system', '2017-07-11'),
@@ -197,18 +178,15 @@ INSERT INTO payment (id, invoice_id, date_paid, sum_id, created_by, created_date
     (7, 8, now() - interval '1 mon + 4 day', 38, 'system', '2017-07-11'),
     (8, 9, now() - interval '2 mon + 11 day', 39, 'system', '2017-07-11'),
     (9, 10, now() - interval '1 mon + 10 day', 40, 'system', '2017-07-11');
-
 INSERT INTO deal_queue (id, queuing_date, priority, deal_id, created_by, created_date) VALUES
     (1, now() - interval '3 day', 'NORMAL', 2, 'system', '2017-07-11'),
     (2, now() - interval '3 day', 'NORMAL', 3, 'system', '2017-07-11'),
     (3, now() - interval '4 day', 'NORMAL', 4, 'system', '2017-07-11');
-
 INSERT INTO salary(id, date_salary, date_from, date_to, employee_id, paid, total_amount_id) VALUES
     (1, now() - interval '1 day',  now() - interval '4 mon', now() - interval '1 mon', 1, TRUE, 41),
     (2, now() - interval '1 day',  now() - interval '3 mon + 10 day', now() - interval '11 day', 4, TRUE, 42),
     (3, now() - interval '1 day',  now() - interval '1 mon + 4 day', now() - interval '5 day', 2, TRUE, 43),
     (4, now() - interval '1 day',  now() - interval '2 mon + 10 day', now() - interval '10 day', 3, TRUE, 44);
-
 INSERT INTO salary_item(id, salary_id, employee_id, invoice_id, employee_payment_id, date_from, date_to, accounted) VALUES
     (1, null, 1, 1, 22, now() - interval '4 mon', now() - interval '3 mon', FALSE),
     (2, null, 1, 2, 22, now() - interval '3 mon', now() - interval '2 mon', FALSE),
