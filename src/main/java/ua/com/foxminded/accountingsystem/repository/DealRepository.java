0dda49c1e0113b52035e14b758a8bccec69fd3f0
Dmitriy Tkachenko <smartphone000014@gmail.com>
2017-08-30 23:26:10 +0300

    feat_231

--- src/main/java/ua/com/foxminded/accountingsystem/repository/DealRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/DealRepository.java
@@ -19,10 +19,10 @@ public interface DealRepository extends JpaRepository<Deal, Long>{
         + "(d.id, d.consultancy.name, d.status, d.openDate, e.id, e.firstName, e.lastName) "
         + "from  Contract c inner join c.employee e right outer join c.deal d "
         + "where d.client.id = ?1 "
-        + "and (c.contractDate  = (select max(cMaxDate.contractDate) "
-        + "                        from Contract cMaxDate "
-        + "                        where cMaxDate.deal.client.id = ?1 "
-        + "                        and cMaxDate.deal.id = c.deal.id) "
-        + "     or c.contractDate is null)")
+        + "and (c.id  = (select max(cMaxId.id) "
+        + "                        from Contract cMaxId "
+        + "                        where cMaxId.deal.client.id = ?1 "
+        + "                        and cMaxId.deal.id = c.deal.id) "
+        + "     or c.id is null)")
     List<ClientDealWithRelatedEmployeeDto> findDealsWithRelatedEmployeesByClient(Long clientId);
 }
