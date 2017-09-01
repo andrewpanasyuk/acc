ab38e43ea5553fe1f9312780d20be9f7f143f259
Dmytro Kushnir <kushnir.do@gmail.com>
2017-09-01 14:37:30 +0300

    Version with CustomErrorControllet (Not working)

---
+++ src/main/java/ua/com/foxminded/accountingsystem/web/controller/error/CustomErrorController.java
@@ -0,0 +1,28 @@
+package ua.com.foxminded.accountingsystem.web.controller.error;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @Autowired
+    public CustomErrorController() {
+    }
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
