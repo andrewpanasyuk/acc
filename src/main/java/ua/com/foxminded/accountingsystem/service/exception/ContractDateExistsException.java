e0738bf1a0cb2c35deb482915c7f36b35a1d87cd
Dmitriy Tkachenko <smartphone000014@gmail.com>
2017-09-02 21:19:29 +0300

    feat_248

--- src/main/java/ua/com/foxminded/accountingsystem/service/exception/ContractDateExistsException.java
+++
@@ -1,7 +0,0 @@
-package ua.com.foxminded.accountingsystem.service.exception;
-
-public class ContractDateExistsException extends RuntimeException {
-    public ContractDateExistsException(String message) {
-        super(message);
-    }
-}
