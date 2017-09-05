d2181eff5ef4d2ea7a5e357b5de5a7e5c75e739c
Eugene Smorodskiy <e.smorodskiy@gmail.com>
2017-09-06 00:28:45 +0300

    move file to gitlab wiki page #252

--- src/main/resources/fm_accounting_db_creation_with_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER fm_accounting WITH PASSWORD 'foxminded';
-
-CREATE DATABASE fm_accounting_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C';
-
-GRANT ALL PRIVILEGES ON DATABASE fm_accounting_db TO fm_accounting;
-
-ALTER DATABASE fm_accounting_db OWNER TO fm_accounting;
