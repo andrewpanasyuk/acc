03760eb2e9ad949695821e46ed142eba6e60b0cd
Dmytro Kushnir <kushnir.do@gmail.com>
2017-09-13 14:24:02 +0300

    Add findAllByDealAndQueuingDate method in DealQueuingRepository

--- src/main/java/ua/com/foxminded/accountingsystem/repository/DealQueueRepository.java
+++ src/main/java/ua/com/foxminded/accountingsystem/repository/DealQueueRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.foxminded.accountingsystem.model.Deal;
 import ua.com.foxminded.accountingsystem.model.DealQueue;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DealQueueRepository extends JpaRepository<DealQueue, Long> {
@@ -11,4 +12,6 @@ public interface DealQueueRepository extends JpaRepository<DealQueue, Long> {
     DealQueue findByDeal(Deal deal);
 
     List<DealQueue> findAllByRemoved(Boolean removed);
+
+    List<DealQueue> findAllByDealAndQueuingDate(Deal deal, LocalDate queuingDate);
 }
