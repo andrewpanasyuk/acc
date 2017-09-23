0eead8d2074ddf2e7ad772a482a9e91de85540de
Dmytro Kushnir <kushnir.do@gmail.com>
2017-09-23 14:38:54 +0300

    Fix

--- src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/DealQueueServiceJPA.java
+++ src/main/java/ua/com/foxminded/accountingsystem/service/serviceJPA/DealQueueServiceJPA.java
@@ -100,7 +100,6 @@ public class DealQueueServiceJPA implements DealQueueService {
         }
 
         dealService.changeStatus(deal, checkedCause);
-        dealQueueRepository.delete(dealQueue);
         dealQueue.setRemoved(true);
         dealQueueRepository.save(dealQueue);
     }
