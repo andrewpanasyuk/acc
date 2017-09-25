8dee5fce5fe4804ab5c4894a364c95e756417ebc
Dmytro Kushnir <kushnir.do@gmail.com>
2017-09-25 22:14:43 +0300

    Added fields to Client model-2

--- src/main/resources/db/migration/V3__alter_client_table_add_registration_form_columns.sql
+++ src/main/resources/db/migration/V3__alter_client_table_add_registration_form_columns.sql
@@ -2,10 +2,10 @@ alter table client add column email VARCHAR(50);
 alter table client add column country VARCHAR(50);
 alter table client add column city VARCHAR(50);
 alter table client add column skype VARCHAR(50);
-alter table client add column phone_number VARCHAR(12);
+alter table client add column phone_number VARCHAR(50);
 
 alter table client_aud add column email VARCHAR(50);
 alter table client_aud add column country VARCHAR(50);
 alter table client_aud add column city VARCHAR(50);
 alter table client_aud add column skype VARCHAR(50);
-alter table client_aud add column phone_number VARCHAR(12);
+alter table client_aud add column phone_number VARCHAR(50);
