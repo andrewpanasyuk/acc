0604acbcdec665686997402f435ee083e1e25ba0
SagaTaras <sahaidachnyi@gmail.com>
2017-09-25 15:56:16 +0300

    Fixed mistake with code formatting

--- src/main/java/ua/com/foxminded/accountingsystem/config/SecurityConfig.java
+++ src/main/java/ua/com/foxminded/accountingsystem/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
