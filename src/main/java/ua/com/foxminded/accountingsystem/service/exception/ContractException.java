e98c0e54aeb563e31aeac100f39a8b128f045c1f
Andrew <avpk77@gmail.com>
2017-09-25 12:31:38 +0300

    temp_1

---
+++ src/main/java/ua/com/foxminded/accountingsystem/service/exception/ContractException.java
@@ -0,0 +1,7 @@
+package ua.com.foxminded.accountingsystem.service.exception;
+
+public class ContractException extends RuntimeException {
+    public ContractException(String message) {
+        super(message);
+    }
+}
